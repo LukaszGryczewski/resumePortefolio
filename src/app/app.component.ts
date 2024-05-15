4176a5f5c3705daf5d9df4d8ce5ffc034000f328
Lukasz <luka.gryczewski@gmail.com>
2024-05-15 18:49:50 +0200

    Import in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,NavigationBarComponent],
+  imports: [RouterOutlet,
+            HeaderComponent,
+            NavigationBarComponent,
+            HomeComponent,
+            ResumeComponent,
+            PortfolioComponent,
+            ContactComponent
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
