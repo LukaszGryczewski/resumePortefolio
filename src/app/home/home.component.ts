b8071b71c7f03cf0f1678d62b0e6b1fad2aaf845
Lukasz <luka.gryczewski@gmail.com>
2024-05-25 20:23:06 +0200

    Create projectService for menage the date of portfolio projects + create the feature project on the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectsService } from '../_services/projects.service';
+import { Project } from '../_models/Project';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +10,15 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor(private titleService : Title){
+export class HomeComponent implements OnInit{
+
+  featureProject = {} as Project;
+
+  constructor(private titleService : Title, private projectsService : ProjectsService){
     this.titleService.setTitle("Lukasz Gryczewski - Home");
+  }
+
+  ngOnInit(): void {
+    this.featureProject = this.projectsService.GetProjectById(0);
   }
 }
