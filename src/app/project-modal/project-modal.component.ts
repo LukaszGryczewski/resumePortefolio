fcceb0069ae4c74f92308147ec4e4c0c5b9e8b34
Lukasz <luka.gryczewski@gmail.com>
2024-06-01 00:28:24 +0200

    Add data project + update css

--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -1,13 +1,16 @@
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Component } from '@angular/core';
 import { Project } from '../_models/Project';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule,
+            CommonModule,],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.scss'
 })
