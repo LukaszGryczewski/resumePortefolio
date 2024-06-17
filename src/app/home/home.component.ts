1b245afe88fb4b611bae115113047e8597ff1584
Lukasz <luka.gryczewski@gmail.com>
2024-06-17 16:08:39 +0200

    Create Resume composer + view + scss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,27 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectsService } from '../_services/projects.service';
 import { Project } from '../_models/Project';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselModule,
-            CommonModule
+  imports: [
+    NgbModule,
+    CommonModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
 
-  featureProject = {} as Project;
+export class HomeComponent implements OnInit {
+  projects: Project[] = [];
+  currentProjectIndex: number = 0;
 
-  constructor(private titleService : Title, private projectsService : ProjectsService){
+  constructor(private titleService: Title, private projectsService: ProjectsService) {
     this.titleService.setTitle("Lukasz Gryczewski - Home");
   }
 
   ngOnInit(): void {
-    this.featureProject = this.projectsService.GetProjectById(0);
+    this.projects = this.projectsService.GetAllProjects();
+  }
+
+  navigateNext(): void {
+    this.currentProjectIndex = (this.currentProjectIndex + 1) % this.projects.length;
+  }
+
+  navigatePrev(): void {
+    this.currentProjectIndex = (this.currentProjectIndex - 1 + this.projects.length) % this.projects.length;
   }
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Renderer2, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { NgbAccordionConfig, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectsService } from '../_services/projects.service';
+
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [NgbAccordionModule,CommonModule],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.scss'
+  styleUrl: './resume.component.scss',
+  providers:[NgbAccordionConfig],
 })
-export class ResumeComponent {
 
-  constructor(private titleService:Title){
+export class ResumeComponent {
+  languages: { name: string, level: number }[] = [];
+  skills: { name: string, level: number }[] = [];
+  technologies: { name: string}[] = [];
+
+  constructor(
+    private titleService: Title,
+    private renderer: Renderer2,
+    private config: NgbAccordionConfig,
+    private projectsService: ProjectsService
+  ) {
     this.titleService.setTitle("Lukasz Gryczewski - Resume");
+  }
+
+  DowloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/documents/Resume-En.pdf');
+    link.setAttribute('download', 'Resume-En.pdf');
+    link.click();
+    link.remove();
+  }
+
+  ngOnInit(): void {
+    this.languages = this.projectsService.languages;
+    this.skills = this.projectsService.skills;
+    this.technologies = this.projectsService.technologies;
   }
 }
